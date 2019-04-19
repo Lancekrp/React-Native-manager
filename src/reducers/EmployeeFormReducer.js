a4a104cddb4e57742d1a5531b91e12bb1ca4cc17
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-19 11:40:48 +0800

    added date function to get current day of week and assign it to shift under Initial_State

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,9 +1,16 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
+const currentDay = () => {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const day =  new Date();
+
+  return daysOfWeek[day.getDay()];
+};
+
 const INITIAL_STATE = {
   name: '',
   phone: '',
-  shift: ''
+  shift: currentDay()
 };
 
 export default (state = INITIAL_STATE, action) => {
