3fd19995da09e9e2b7cf6fc3c04bdaa68d7077e5
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-24 13:36:18 +0800

    styling the modal

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,7 +3,9 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = ({ children, visibe, onAccept, onDecline }) => {
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
     <Modal
       visible={visible}
@@ -11,9 +13,9 @@ const Confirm = ({ children, visibe, onAccept, onDecline }) => {
       animationType="slide"
       onRequestClose={() => {}}
     >
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
         </CardSection>
 
          <CardSection>
@@ -23,6 +25,24 @@ const Confirm = ({ children, visibe, onAccept, onDecline }) => {
       </View>
     </Modal>
   );
+};
+
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
 };
 
 export { Confirm };
