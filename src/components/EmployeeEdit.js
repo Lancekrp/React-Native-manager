ee41102c046614e82821b54a0cf0634be8c175a5
Lance Purnell <lance.purnell@hotmail.fr>
2019-04-24 13:44:00 +0800

    employee delete action creator

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -26,6 +26,14 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept() {
+
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -48,6 +56,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this employee?
         </Confirm>
